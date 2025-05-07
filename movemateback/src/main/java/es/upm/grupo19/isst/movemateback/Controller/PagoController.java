572965a1ec8a7ff55ed6857a341a0eb7437c6b8d
Alvaro Palomino Morales <alvaro.palomino@alumnos.upm.es>
2025-05-07 18:49:46 +0200

    Añadimos ver todos los pagos ccon peticion postman

--- movemateback/src/main/java/es/upm/grupo19/isst/movemateback/Controller/PagoController.java
+++ movemateback/src/main/java/es/upm/grupo19/isst/movemateback/Controller/PagoController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,7 +40,11 @@ public class PagoController {
         this.reservaRepository = reservaRepository;
         this.actividadRepository = actividadRepository;
     }
-
+    // Endpoint para obtener todos los pagos.
+    @GetMapping()
+    public Iterable<Pago> getAllPagos() {
+        return pagoRepository.findAll();
+    }
     // Metodo para pagar una reserva de actividad por parte de un cliente.
     @PostMapping("/{actividadId}/{clienteId}")
     public ResponseEntity<?> pagarReserva(@PathVariable Long actividadId, @PathVariable Long clienteId,
